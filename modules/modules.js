ca5ab2ef9d275d01dfd55a0446f7424d1be8396a
hijiri <crew.sento0864@gmail.com>
2025-01-10 23:40:09 +0900

    rotate以外のロジック完成

--- modules/modules.js
+++ modules/modules.js
@@ -24,71 +24,63 @@ export const hum = () => {
 // －－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－
 
 export const icon = () => {
- //表示切替用DOM取得
- const iconImg = document.querySelector('.icon-img');
- const iconDes = document.querySelector('.icon-description');
- //ボタンDOM取得
- const button = {
-  text:document.querySelector('.btn-text');
-  color:document.querySelector('.btn-color');
-  big:document.querySelector('.btn-big');
-  small:document.querySelector('.btn-small');
-  rotate:document.querySelector('.btn-rotate');
- };
- // icon-descriptionに表示するテキスト
- const iconText = [
-  "This is JavaScript icon!",
-  "Wow!!",
-  "Great!!!",
-  "Amazing!!!!",
-  "Oh my god!!!!!"
- ];
-// textボタン　イベント処理のロジック
- let textIndex = 0;
-　iconDes.innerText = iconText[0];
-  button.text.addEventLisner('click',()=>{
-  textIndex = (textIndex+1)%iconText.length;
-  iconDes.innerText = iconText[textIndex];
- });
-};
-// icon-descriptionの文字色
- const iconColor = [
-  "#121212",
-  "gold",
-  "red",
-  "blue",
-  "green"
- ];
-// colorボタン　イベント処理のロジック
- let colorIndex = 0;
-　iconDes.style.color = iconColor[0];
-  button.color.addEventLisner('click',()=>{
-  colorIndex = (colorIndex+1)%iconColor.length;
-  iconDes.style.color = iconColor[colorIndex];
- });
-};
-// big,smallボタンで動作するscale倍率の初期値
-　let scaleIndex = 1;
-// bigボタン　イベント処理のロジック
-  button.big.addEventLisner('click',()=>{
-   if(scaleIndex<1.8){
-    scaleIndex=scaleIndex+0.2;
-    iconImg.style.transform = scale(${scaleIndex});
-   }else{
-    alert("もうムリですいね");
-   }
+  //表示切替用DOM取得
+  const iconImg = document.querySelector('.icon-img');
+  const iconDes = document.querySelector('.icon-description');
+  //ボタンDOM取得
+  const button = {
+    text: document.querySelector('.btn-text'),
+    color: document.querySelector('.btn-color'),
+    big: document.querySelector('.btn-big'),
+    small: document.querySelector('.btn-small'),
+    rotate: document.querySelector('.btn-rotate'),
+  };
+  // icon-descriptionに表示するテキスト
+  const iconText = [
+    'This is JavaScript icon!',
+    'Wow!!',
+    'Great!!!',
+    'Amazing!!!!',
+    'Oh my god!!!!!',
+  ];
+  // textボタン　イベント処理のロジック
+  let textIndex = 0;
+  iconDes.innerText = iconText[0];
+  button.text.addEventListener('click', ()=> {
+    textIndex = (textIndex + 1) % iconText.length;
+    iconDes.innerText = iconText[textIndex];
   });
-// smallボタン　イベント処理のロジック
-  button.small.addEventLisner('click',()=>{
-   if(scaleIndex>0.2){
-    scaleIndex=scaleIndex-0.2;
-    iconImg.style.transform = scale(${scaleIndex});
-   }else{
-    alert("もうムリですいね");
-   }
+  // icon-descriptionの文字色
+  const iconColor = ['#121212', 'gold', 'red', 'blue', 'green'];
+  // colorボタン　イベント処理のロジック
+  let colorIndex = 0;
+  iconDes.style.color = iconColor[0];
+  button.color.addEventListener('click', () => {
+    colorIndex = (colorIndex + 1) % iconColor.length;
+    iconDes.style.color = iconColor[colorIndex];
   });
-// rotateボタン　イベント処理のロジック
-  button.rotate.addEventLisner('click',()=>{
-    iconImg.style.transform = rotate(1080deg);
+  // big,smallボタンで動作するscale倍率の初期値
+  let scaleIndex = 1;
+  // bigボタン　イベント処理のロジック
+  button.big.addEventListener('click', () => {
+    if (scaleIndex < 1.6) {
+      scaleIndex = scaleIndex + 0.2;
+      iconImg.style.transform = `scale(${scaleIndex})`;
+    } else {
+      alert('ボタンが隠れちゃうよ');
+    }
+  });
+  // smallボタン　イベント処理のロジック
+  button.small.addEventListener('click', () => {
+    if (scaleIndex > 0.4) {
+      scaleIndex = scaleIndex - 0.2;
+      iconImg.style.transform = `scale(${scaleIndex})`;
+    } else {
+      alert('消えちゃうよ');
+    }
+  });
+  // rotateボタン　イベント処理のロジック
+  button.rotate.addEventListener('click', () => {
+    iconImg.style.transform = 'rotate(1080deg)';
   });
 };
