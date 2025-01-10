b1f1452579db3911b1c6c5a779a72805eb489355
SENTO <crew.sento0864@gmail.com>
2025-01-10 12:56:07 +0900

    The iconのボタンのロジック追記

--- modules/modules.js
+++ modules/modules.js
@@ -27,13 +27,55 @@ export const icon = () => {
  const iconDes = document.querySelector('.icon-description');
  //ボタンDOM取得
  const button = {
-  text:document.querySelector('1');
-  color:document.querySelector('2');
-  small:document.querySelector('3');
-  big:document.querySelector('4');
-  rotate:document.querySelector('5');
+  text:document.querySelector('text');
+  color:document.querySelector('color');
+  small:document.querySelector('small');
+  big:document.querySelector('big');
+  rotate:document.querySelector('rotate');
  }
- 1.addEventLisner('click',()=>{
-  iconDes.innerText=;
+ const iconText = [
+   "This is JavaScript icon!",
+   "Wow!!",
+   "Great!!!",
+   "Amazing!!!!",
+   "Oh my god!!!!!"
+ ];
+ const iconColor = [
+   "black",
+   "red",
+   "bule",
+   "green",
+   "gold"
+ ];
+ button.text.addEventLisner('click',()=>{
+   for(let number = 0 ; number<iconText.length ; number++){
+   iconDes.innerText = iconText[number];
+   if(number === iconText.length-1){
+     number = 0;
+   }
+  }
  });
-};
+ button.color.addEventLisner('click',()=>{
+   for(let number = 0 ; number<iconColor.length ; number++){
+   iconDes.style.color = iconColor[number];
+   if(number === iconText.length-1){
+     number = 0;
+   }
+  }
+  });
+ button.small.addEventLisner('click',()=>{
+   for(let number = 1 ; number>0.2 ; number-0.2){
+   iconImg.style.transform = scale(${number});
+   if(number === 0.2){
+     number = 0;
+   }
+  }
+  });
+ button.big.addEventLisner('click',()=>{
+   for(let number = 1 ; number<0.8 ; number+0.2){
+   iconImg.style.transform = scale(${number});
+   if(number === 0.8){
+     number = 0;
+   }
+  }
+  });
